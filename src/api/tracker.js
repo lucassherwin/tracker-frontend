f11458947b998199ab7b131968e7cb612d714129
lucassherwin <lksherwin@gmail.com>
2021-03-18 17:05:19 -0400

    created navigationRef

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://283bfc697562.ngrok.io'
+  baseURL: 'http://eddad9f8d24f.ngrok.io'
 })
 
 // npm run dev -> ngrok http 3000 to get new url
