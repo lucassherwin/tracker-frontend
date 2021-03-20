558ee1ff75f7e0859d1fad0870bd1204394e62e3
lucassherwin <lksherwin@gmail.com>
2021-03-20 19:07:26 -0400

    created function to request permission to track users location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
-import Map from '../components/Map';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { requestPermissionsAsync } from 'expo-location';
+import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermissionsAsync();
+      if (!granted) 
+      {
+        throw new Error('Location permission not granted');
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
       <Text h2>Create a track</Text>
       <Map />
+      {err ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
   );
 };
@@ -16,3 +36,4 @@ const TrackCreateScreen = () => {
 const styles = StyleSheet.create({});
 
 export default TrackCreateScreen;
+
