c93c26190b3c6d9095267f7c71842c6f1e22610c
lucassherwin <lksherwin@gmail.com>
2021-03-23 17:24:58 -0400

    updated location context and TrackCreateScreen to update when the user presses the start recording button

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://1ef181eeaa19.ngrok.io'
+  baseURL: 'http://0bcd45d12ad3.ngrok.io'
 })
 
 // npm run dev -> ngrok http 3000 to get new url
