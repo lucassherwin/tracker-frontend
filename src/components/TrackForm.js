b3f31f1898f9fe5e49a0ccd1b9073a074fa2f7e5
lucassherwin <lksherwin@gmail.com>
2021-03-23 18:30:24 -0400

    updated track form

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
-import Spacer from './Spacer';
+// import Spacer from './Spacer';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackForm = () => {
+  const { state: { name, recording, locations }, startRecording, stopRecording, changeName } = useContext(LocationContext);
+
+  console.log(locations.length);
+
   return (
     <>
-      <Input placeholder='Enter track name' />
-      <Button title='Start Recording' />
+      <Input placeholder='Enter track name' onChangeText={changeName} value={name} />
+      {
+        recording ? <Button title='Stop Recording' onPress={stopRecording} />
+        :
+        <Button title='Start Recording' onPress={startRecording} /> 
+      }
     </>
   );
 };
